e9cff82282d58c09e80b125f7bb7f9fa2f408ecd
CliffordArmstead <143453769+CliffordArmstead@users.noreply.github.com>
2026-04-15 22:26:04 -0400

    Create 2_insert_data.sql

---
+++ 2_insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Student VALUES (...);
+INSERT INTO Course VALUES (...);
+INSERT INTO Enrollment VALUES (...);
+INSERT INTO Category VALUES (...);
+INSERT INTO Assignment VALUES (...);
+INSERT INTO Score VALUES (...);
